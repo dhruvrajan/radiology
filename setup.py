134a0f08f20f10971480a498e7f96dbaceaebd67
Dhruv Rajan <dhruv@krishnaprem.com>
2019-09-10 11:06:00 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='radiology-nlp',
+      version='0.1',
+      description='Experiments and code for processing radiology reports',
+      url='http://github.com/dhruvrajan/radiology-nlp',
+      author='Dhruv Rajan',
+      author_email='dhruv@cs.utexas.edu',
+      license='MIT',
+      packages=['radiology-nlp'],
+      zip_safe=False)
