1e89789ecc4493cee93dd59dde953f3fd7b7b708
Dhruv Rajan <dhruv@krishnaprem.com>
2019-09-25 12:07:35 -0500

    types for diseases

--- radiology_nlp/params.py
+++
@@ -1,4 +0,0 @@
-
-LABELED_REPORTS_PATH = "data/labeled_neuro_reports/reports/"
-UNLABELED_REPORTS_PATH = "data/unlabeled_neuro_reports/reports/"
-UNLABELED_DISEASES = UNLABELED_REPORTS_PATH + "diseases.csv"
--- radiology_nlp/utils.py
+++
@@ -1,5 +0,0 @@
-from typing import Any, Dict
-
-
-def case(item: Any, options: Dict[Any, Any]) -> Any:
-    return options[item]
