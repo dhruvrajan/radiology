0619cdb836a20015968c7cda934ebf1af74b11f0
Dhruv Rajan <dhruv@krishnaprem.com>
2019-09-28 17:15:02 -0500

    consolidating diseases

--- radiology/params.py
+++ radiology/params.py
@@ -4,5 +4,8 @@ UNLABELED_REPORTS_PATH = "data/unlabeled_neuro_reports/reports/"
 UNLABELED_DISEASES = UNLABELED_REPORTS_PATH + "diseases.csv"
 KF_LABELS_PATH = "data/labeled_neuro_reports/kf_ground_truth_commas.csv"
 
+ORIGINAL_BN = "data/bayes_network/original/renamed_network.csv"
+HYBRID_BN = "data/"
+
 # Symbols
 UNK_SYMBOL = "<UNK>"
--- radiology/types/kf_labels.py
+++ radiology/types/kf_labels.py
@@ -2,8 +2,12 @@ import pandas as pd
 from radiology.params import KF_LABELS_PATH
 
 
-def read_kf_labels(kf_path: str=KF_LABELS_PATH):
+def read_raw_kf_labels(kf_path: str = KF_LABELS_PATH):
     with open(kf_path) as f:
-        kf = pd.read_csv(f)
+        return pd.read_csv(f)
 
+def read_cleaned_kf_labels(kf_path: str = KF_LABELS_PATH):
     
+
+if __name__ == "__main__":
+    print(read_kf_labels())
